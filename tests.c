ab1aa652964e9ae523625073053699d4df8b8e2c
Emilia <emiliaglibowska@gmail.com>
2023-12-01 07:23:59 +0100

    Curses

--- tests.c
+++ tests.c
@@ -1,20 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 
-int main(int argc, char ** argv) {
-    double min_val = argc > 1 ? atof(argv[1]) : 0;
-    double max_val = argc > 2 ? atof(argv[2]) : 10;
-    double val_delta = max_val - min_val;
-    int size = argc > 3 ? atoi(argv[3]) : 10;
-    int i;
-    double *v = malloc(size * sizeof(double));
-    srand(time(NULL));
-    for(i = 0; i < size; i++)
-        v[i] = min_val + ((double)rand()) / RAND_MAX * val_delta;
-    printf("[");
-    for(i = 0; i < size - 1; i++)
-        printf("%g, ", v[i]);
-    printf("%g]\n", v[size - 1]);
-    
+#define także_ten return
+#define prawda ;
+#define yyy ;
+#define równe =
+#define którego .
+#define a_w_nim .
+#define wskaźnik *
+#define dla for
+#define zwiększ_o +=
+#define zwiększ ++
+#define zmniejsz --
+#define wartość_całkowita int
+#define wartość_zmiennoprzecinkowa double
+#define znak char
+#define napis char*
+#define pustka void
+#define struktura struct
+#define definiuj_typ typedef
+#define rozmiar sizeof
+#define NIC NULL
+#define adres &
+#define gdy while
+#define rób do
+
+definiuj_typ struktura produkt
+{
+    napis nazwa prawda
+    napis opis prawda
+    wartość_zmiennoprzecinkowa cena prawda
+    wartość_całkowita ilość prawda
+} typ_produkt prawda
+
+wartość_zmiennoprzecinkowa weź_cenę_całkowitą(wartość_całkowita długość, typ_produkt wskaźnik produkty) {
+    wartość_całkowita i prawda
+    wartość_zmiennoprzecinkowa suma prawda
+    dla (i równe 0 prawda i < długość prawda zwiększ i)
+        suma zwiększ_o i[produkty] a_w_nim cena * i[produkty] a_w_nim ilość prawda
+    także_ten suma prawda
+}
+
+wartość_całkowita main(pustka) {
+    typ_produkt wskaźnik produkty równe malloc(rozmiar(typ_produkt) * 3) prawda
+    0[produkty] którego cena równe 1.20 yyy 0[produkty] którego ilość równe 8 prawda
+    1[produkty] którego cena równe 3.49 yyy 1[produkty] którego ilość równe 2 prawda
+    2[produkty] którego cena równe 6.39 yyy 2[produkty] którego ilość równe 5 prawda
+    printf("%g\n", weź_cenę_całkowitą(3, produkty)) prawda
+    także_ten 0 prawda
 }
